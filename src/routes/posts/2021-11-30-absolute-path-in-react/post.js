d33437725fef1e3b85735d47c3f3d1cf1cb7b7a0
addTvb <arslan.web.dev@gmail.com>
2021-11-30 17:17:44 +0500

    🌈style: change style to balck theme and my own accent colors, add new article

---
+++ src/routes/posts/2021-11-30-absolute-path-in-react/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Создаем абсолютные пути к файлам в React`,
+  tags: ['react', 'js', 'ts'],
+  spoiler: "Это самый быстрый способ добавить абсолютные пути в ваш проект, без каких либо зависимостей☝",
+  getContent: () => import('./document.mdx'),
+}
--- src/routes/posts/2021-11-30-css-avatar-radius/post.js
+++ src/routes/posts/2021-11-30-css-avatar-radius/post.js
@@ -1,6 +1,6 @@
 export default {
   title: `Скругление аватарки правильно`,
-  tags: ['html', 'css'],
+  tags: ['copypaste', 'css'],
   spoiler: "Скругление фото аватарки, без сss багов",
   getContent: () => import('./document.mdx'),
 }
