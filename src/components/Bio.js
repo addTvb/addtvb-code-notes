2423743575a8b78a055537f405a9aafb79d9c84d
addTvb <arslan.web.dev@gmail.com>
2021-11-30 14:20:52 +0500

    Initial commit

---
+++ src/components/Bio.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./Bio.module.css";
+import Photo from "../components/Bio/img/arslan-abayev.jpg";
+
+function Bio(props) {
+    return (
+        <div
+            className={`
+      ${styles.Bio}
+      ${props.className || ""}
+    `}
+        >
+            <img
+                src={Photo}
+                alt="Abaev Arslan smiles at the photo in a blue shirt"
+            />
+            <p>
+                Небольшой блог с заметками в виде применимых на практике
+                кусочков кода для разных нужд, этакий блог шпаргалка👨‍💻😅.
+                <br />
+                Блог был создан с помощью:{" "}
+                <a href="https://github.com/jamesknelson/create-react-blog">
+                    create-react-blog
+                </a>
+                <br />
+                Автор:{" "}
+                <a href="https://www.linkedin.com/in/arslan-abayev-addtvb/">
+                    Abayev Arslan
+                </a>{" "}
+                aka{" "}
+                <a href="https://www.linkedin.com/in/arslan-abayev-addtvb/">
+                    addTvb
+                </a>
+                <br />
+            </p>
+        </div>
+    );
+}
+
+export default Bio;
---
+++ src/siteMetadata.js
@@ -0,0 +1,12 @@
+export default {
+    // The blog's title as it appears in the layout header, and in the document
+    // <title> tag.
+    title: "addTvb-code-notes",
+
+    author: "Abayev Arslan",
+    description:
+        "Небольшой блог с заметками в виде применимых на практике кусочков кода для разных нужд, этакий блог шпаргалка👨‍💻😅",
+
+    // The number of posts to a page on the site index.
+    indexPageSize: 10,
+};
