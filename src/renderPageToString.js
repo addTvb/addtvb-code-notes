45afd9307802a2bf69e8dddd55e6ae097b072497
addTvb <arslan.web.dev@gmail.com>
2021-12-04 12:04:16 +0500

    ✨feat: added editorconfig and gh-actions

--- src/renderPageToString.js
+++ src/renderPageToString.js
@@ -1,16 +1,16 @@
-import renderReactPageToString from 'react-navi/create-react-app'
-import renderRSSFeedToString from './renderRSSFeedToString'
+import renderReactPageToString from "react-navi/create-react-app";
+import renderRSSFeedToString from "./renderRSSFeedToString";
 
 /**
  * navi-scripts will call this function for each of your site's pages
  * to produce its statically rendered HTML.
  */
 async function renderPageToString(props) {
-  if (props.url.pathname === '/rss') {
-    return await renderRSSFeedToString(props)
-  }
+	if (props.url.pathname === "/rss") {
+		return await renderRSSFeedToString(props);
+	}
 
-  return renderReactPageToString(props)
+	return renderReactPageToString(props);
 }
 
-export default renderPageToString
+export default renderPageToString;
