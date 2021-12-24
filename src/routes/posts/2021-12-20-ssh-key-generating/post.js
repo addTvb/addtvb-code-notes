ed8a8da739a15677211b1348cb05d513d13f399c
addTvb <arslan.web.dev@gmail.com>
2021-12-24 22:17:26 +0500

    Added two articles

---
+++ src/routes/posts/2021-12-20-ssh-key-generating/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Быстро создаем ssh ключ`,
+  tags: ['git', 'ssh', 'github'],
+  spoiler: "Покажу как быстро создать ssh ключ с паролем и сразу привяжем его к github",
+  getContent: () => import('./document.mdx'),
+}
---
+++ src/routes/posts/2021-12-23-regex-cheat-sheet/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Шпаргалка по regex`,
+  tags: ['copypaste', 'cheatsheet'],
+  spoiler: "Готовые регулярки, и шпаргалка по созданию своей регулярки",
+  getContent: () => import('./document.mdx'),
+}
