d24501717b24872e32e6540701600133007e5d43
addTvb <arslan.web.dev@gmail.com>
2022-01-21 11:32:32 +0500

    ✨feat: write an article

--- src/routes/posts/2021-12-23-regex-cheat-sheet/post.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: `Шпаргалка по regex`,
-  tags: ['copypaste', 'cheatsheet'],
-  spoiler: "Готовые регулярки, и шпаргалка по созданию своей регулярки",
-  getContent: () => import('./document.mdx'),
-}
