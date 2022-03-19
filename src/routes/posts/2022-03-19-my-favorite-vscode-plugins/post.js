9cd86ebe4b3fa576e47c7a3ff2cf513db41a07e2
addTvb <arslan.web.dev@gmail.com>
2022-03-19 22:44:14 +0500

    ✨feat: add new article

---
+++ src/routes/posts/2022-03-19-my-favorite-vscode-plugins/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Мой топ 10 плагинов для VSCode`,
+  tags: ['vscode'],
+  spoiler: "Подборка нацелена больше на Frontend в целом чем на React.js",
+  getContent: () => import('./document.mdx'),
+}
