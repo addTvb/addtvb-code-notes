aaab8780d7480c5f678a1b02c650aa8302e1b016
Arslan Abayev <abayew.arslan@gmail.com>
2022-05-02 13:24:37 +0500

    add article about create-react-app

---
+++ src/routes/posts/2022-05-02-how-to-create-react-app/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Создаем шаблон React приложения`,
+  tags: ['react', 'js'],
+  spoiler: "Пошаговый план по созданию React приложения",
+  getContent: () => import('./document.mdx'),
+}
